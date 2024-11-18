9c7ce645864c04344bf779bcecd5fa3c2f6f0586
wootusNootus <migospodinoff@gmail.com>
2024-11-17 22:29:28 -0600

    getting gradle to work

---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,59 @@
+public class SinglyLinkedList {
+
+    Node head;
+
+    static class Node {
+        ChainLink data;
+        Node next;
+        
+        // Recursion
+        Node(ChainLink d) {
+          data = d;
+          next = null;
+        }
+    }
+
+    public static void insert(SinglyLinkedList list, ChainLink data) {
+        Node new_node = new Node(data);
+
+        if (list.head == null) {
+            list.head = new_node;
+        } 
+        else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+            last.next = new_node;
+        }
+    }
+
+    public static ChainLink get(SinglyLinkedList list, int index) {
+        Node currentNode = list.head;
+        int count = 0;
+    
+        while (currentNode != null) {
+            if (count == index) {
+                return currentNode.data; 
+            }
+            currentNode = currentNode.next;
+            count++;
+        }
+    
+        return null;
+    }
+    
+
+    public static void printList(SinglyLinkedList list) {
+        System.out.println("Printing the list:");
+        Node currentNode = list.head;
+
+        while (currentNode != null) {
+            System.out.println(currentNode.data.color + " ");
+            currentNode = currentNode.next;
+        }
+
+        System.out.println();
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,34 @@
+public class Test {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        ChainLink link1 = new ChainLink("Red");
+        ChainLink link2 = new ChainLink("Blue");
+        ChainLink link3 = new ChainLink("Green");
+
+        // Set/insert elements
+        SinglyLinkedList.insert(list, link1);
+        SinglyLinkedList.insert(list, link2);
+        SinglyLinkedList.insert(list, link3);
+
+        // Print the list
+        System.out.println("Printing list:");
+        SinglyLinkedList.printList(list);
+
+        // Get elements
+        ChainLink result = SinglyLinkedList.get(list, 1);
+        if (result != null) {
+            System.out.println("Element at index 1: " + result.color);
+        } else {
+            System.out.println("Index 1 is out of bounds.");
+        }
+
+        result = SinglyLinkedList.get(list, 5); // Example of out-of-bounds access
+        if (result != null) {
+            System.out.println("Element at index 5: " + result.color);
+        } else {
+            System.out.println("Index 5 is out of bounds.");
+        }
+    }
+}
+
