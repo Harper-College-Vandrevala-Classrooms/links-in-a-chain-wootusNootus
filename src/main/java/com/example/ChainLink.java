48bbf9ae69f47861323eedce1c6f1564458ed679
wootusNootus <migospodinoff@gmail.com>
2024-11-17 22:42:27 -0600

    Changed project structure

--- src/main/java/com/example/ChainLink.java
+++ src/main/java/com/example/ChainLink.java
@@ -1,3 +1,4 @@
+package com.example;
 public class ChainLink {
     public String color;
 
--- src/test/java/com/example/Test.java
+++ src/test/java/com/example/Test.java
@@ -1,3 +1,4 @@
+package com.example;
 public class Test {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
@@ -19,14 +20,16 @@ public class Test {
         ChainLink result = SinglyLinkedList.get(list, 1);
         if (result != null) {
             System.out.println("Element at index 1: " + result.color);
-        } else {
+        } 
+        else {
             System.out.println("Index 1 is out of bounds.");
         }
 
-        result = SinglyLinkedList.get(list, 5); // Example of out-of-bounds access
+        result = SinglyLinkedList.get(list, 5);
         if (result != null) {
             System.out.println("Element at index 5: " + result.color);
-        } else {
+        } 
+        else {
             System.out.println("Index 5 is out of bounds.");
         }
     }
